50c1922f7094cbd8e8a8dfe22bac631693d4bc38
Aman0246 <amankashyap0246@gmail.com>
2023-09-05 01:07:07 +0530

    done

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -1,44 +1,71 @@
-import { Box , FormControl, TextField } from '@mui/material'
+import { Box, FormControl, TextField } from '@mui/material'
 import LoadingButton from '@mui/lab/LoadingButton';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { motion } from "framer-motion"
 import toast from 'react-hot-toast';
 import validator from 'validator';
+import './a.css'
 
-export default function Home() {
+export default function Home({dark,setdark}) {
   const [input, setinput] = useState()
   const [buttonLoding, setbuttonLoding] = useState(false)
+  const [rows, setRows] = useState(6);
+  useEffect(() => {
+    // Function to update the number of rows based on window width
+    function updateRows() {
+      if (window.innerWidth < 400) {
+        console.log(window.innerWidth)
+        setRows(10); // Set rows to 10 when the window width is 400px or less
+      }
+      else{
+        setRows(6)
+      }
+    }
+
+    window.addEventListener('resize', updateRows);
+
+    updateRows()
+    return () => {
+      window.removeEventListener('resize', updateRows);
+    };
+
+  }, [])
   let handleChange = (e) => {
 
     setinput({ ...input, [e.target.name]: e.target.value })
   }
 
-  const handleSubmit = async() => {
+  const handleSubmit = async () => {
     setbuttonLoding(true)
-    if(input==undefined ){
+    if (input == undefined) {
       setbuttonLoding(false)
-      return toast.error('Empty Fields') }
-    if(input.name==undefined || input.name==""  ){
+      return toast.error('Empty Fields')
+    }
+    if (input.name == undefined || input.name == "") {
       setbuttonLoding(false)
-      return toast.error('Name is empty') }
-    if(input.email==undefined  || input.email==""  ){
+      return toast.error('Name is empty')
+    }
+    if (input.email == undefined || input.email == "") {
       setbuttonLoding(false)
-      return toast.error('Email is empty') }
-    if(!validator.isEmail(input.email)) {
+      return toast.error('Email is empty')
+    }
+    if (!validator.isEmail(input.email)) {
       setbuttonLoding(false)
-      return toast.error('Invalid Email ') }
-    if(input.subject==undefined || input.subject==""   ){
+      return toast.error('Invalid Email ')
+    }
+    if (input.subject == undefined || input.subject == "") {
       setbuttonLoding(false)
-      return toast.error('Subject is empty') }
-    await axios.post('/sendMail',{...input,message}).then((e)=>{
+      return toast.error('Subject is empty')
+    }
+    await axios.post('/sendMail', { ...input, message }).then((e) => {
       setbuttonLoding(false)
-     
-      if(e.data.status == true){
-        
+
+      if (e.data.status == true) {
+
         return toast.success(e.data.message)
       }
-      else{return toast.error('Server Error')}
+      else { return toast.error('Server Error') }
     })
   }
   const [message, setMessage] = useState(`Our newsletter is our gateway to:
@@ -55,33 +82,36 @@ export default function Home() {
   If you have any questions or need further assistance, please feel free to contact our support team at zealyug.support@gmail.com.
 
   Best regards,
-  {name}
+  [name]
   amankashyap0246@gmail.com`)
   return (
-    <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0 }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '85vh', marginTop: '3rem' }}>
-      <Box sx={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px', border: '2px solid gray', height: '80%', padding: '1rem', borderRadius: '10px', display: 'flex', flexDirection: 'column', width: '60%', gap: '1rem' }}>
-        <Box sx={{flex:1,display:'flex',flexDirection:'column'}}>
-        <TextField onChange={handleChange} name='name' type='email' sx={{ fontSize: '8px', flex: 1 }} id="standard-basic" label="Name" variant="standard" />
-        <TextField onChange={handleChange} name='email' sx={{ flex: 1 }} id="standard-basic" label="Email" type='email' variant="standard" />
-        <TextField onChange={handleChange} name='subject' sx={{ flex: 1 }} id="standard-basic" type='email'   label="Subject" variant="standard" />
+    <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh',width:'100%'}}>
+
+    <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} exit={{ opacity: 0, scale: 0 }} style={{ display: 'flex',backgroundColor:dark?"":'rgb(13 5 77)',width:'100%',height:'100%', justifyContent: 'center',paddingTop:"1rem", alignItems: 'center', marginTop: '3rem' }}>
+      <Box sx={{ '@media (max-width: 400px)': { fontSize: '10px', width: '95%' }, boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px', border: '2px solid gray', height: '80%', padding: '1rem', borderRadius: '10px', display: 'flex', flexDirection: 'column', width: '50%', gap: '1rem' }}>
+        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+          <TextField onChange={handleChange} name='name' type='email' sx={{ fontSize: '8px', flex: 1,"& > label":{color:dark?"black":'white'},"& > div>input":{color:dark?"black":'#8b8a8a'}}} id="standard-basic" label="Name" variant="standard" />
+          <TextField onChange={handleChange}  name='email' sx={{ flex: 1,"& > label":{color:dark?"black":'white'},"& > div>input":{color:dark?"black":'#8b8a8a'} }} id="standard-basic" label="Email" type='email' variant="standard" />
+          <TextField onChange={handleChange} name='subject' sx={{ flex: 1,"& > label":{color:dark?"black":'white'},"& > div>input":{color:dark?"black":'#8b8a8a'} }} id="standard-basic" type='email' label="Subject" variant="standard" />
         </Box>
-        <Box sx={{flex:5,display:'flex'}}>
-        <TextField onChange={(e) => setMessage(e.target.value)} sx={{ flex: 4 }}
-          name='message'
-          id="standard-multiline-static"
-          label="Text"
-          multiline
-          rows={6}
-          value={message}
-          variant="standard"
-        />
+        <Box sx={{ flex: 5, display: 'flex' }}>
+          <TextField onChange={(e) => setMessage(e.target.value)} sx={{ flex: 4,"& > label":{color:dark?"black":'#008eff'},"& > div >textarea":{color:dark?"black":'#8b8a8a'} }}
+            name='message'
+            id="standard-multiline-static"
+            label="Text"
+            multiline
+            rows={rows}
+            value={message}
+            variant="standard"
+            />
         </Box>
 
 
-       
-        <LoadingButton loading={buttonLoding}  onClick={handleSubmit} variant="contained">Send</LoadingButton>
+
+        <LoadingButton loading={buttonLoding} onClick={handleSubmit} variant="contained">Send</LoadingButton>
 
       </Box>
     </motion.div>
+            </Box>
   )
 }
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -4,18 +4,28 @@ import AppBar from '@mui/material/AppBar';
 import GmailLogo from './image/gmail.png'
 import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 import { Link } from 'react-router-dom';
+import Switch from '@mui/material/Switch';
 
-export default function Navbar() {
+export default function Navbar({dark,setdark}) {
+    const handleDarkModeChange = (e) => {
+        setdark(!dark); 
+      };
     return (
         <Box sx={{ color: "black" }}>
-            <AppBar sx={{ bgcolor: '#e8e8e8', color: 'black', height: '50px' }}>
+            <AppBar sx={{ bgcolor: dark?'#e8e8e8':'#160f4f', color: dark?"black":'white', height: '50px' }}>
                 <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+
                     <Box title="Gmail" sx={{ display: 'flex', alignItems: 'center' }}>
                         <img style={{ width: '2rem' }} src={GmailLogo} alt="Gmail Logo" />
                     </Box>
-                    <Box title="Write Gmail" sx={{ alignSelf: 'center' }}>
+                    <Box title="Write Gmail" sx={{ display:'flex',alignItems:'center',gap:'5rem' }}>
+                        <Switch title="changeMode"  onChange={handleDarkModeChange} />
+                        <Link to={'/allmails'}>
+
+                        <Button sx={{textTransform: 'none',width:'100%',padding:'1px',color:dark?"black":'white',backgroundColor:dark?"#dacdcd":'black'}}> Mails</Button>
+                        </Link>
                         <Link to={'/'}>
-                            <ModeEditOutlineIcon sx={{ cursor: 'pointer' }} >write mail</ModeEditOutlineIcon>
+                            <ModeEditOutlineIcon sx={{ cursor: 'pointer',color:dark?"black":'white' }} >write mail</ModeEditOutlineIcon>
                         </Link>
                     </Box>
                 </Toolbar>
